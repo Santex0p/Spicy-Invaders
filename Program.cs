8c04a43adab72cccd529af5573e4e8729a66e634
Santiago Escobar Toro <pn63pgk@eduvaud.ch>
2024-01-18 15:53:54 +0100

    Firts Version

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spicy_Invaders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "SCORE <> " + "HI-SCORE " + "SCORE <>" + "\n" +
+                "0000 " + "0000");
+
+            Console.ReadLine();
+
+        }
+    }
+}
